29c2191303a24c56029e0ac5fa2931b7b2e9ce16
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:43:30 +0530

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelid = new System.Windows.Forms.Label();
+            this.labelname = new System.Windows.Forms.Label();
+            this.labelsal = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnwrite = new System.Windows.Forms.Button();
+            this.btnread = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelid
+            // 
+            this.labelid.AutoSize = true;
+            this.labelid.Location = new System.Drawing.Point(76, 54);
+            this.labelid.Name = "labelid";
+            this.labelid.Size = new System.Drawing.Size(91, 20);
+            this.labelid.TabIndex = 0;
+            this.labelid.Text = "Employeeid";
+            this.labelid.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelname
+            // 
+            this.labelname.AutoSize = true;
+            this.labelname.Location = new System.Drawing.Point(76, 112);
+            this.labelname.Name = "labelname";
+            this.labelname.Size = new System.Drawing.Size(119, 20);
+            this.labelname.TabIndex = 1;
+            this.labelname.Text = "Employeename";
+            // 
+            // labelsal
+            // 
+            this.labelsal.AutoSize = true;
+            this.labelsal.Location = new System.Drawing.Point(76, 185);
+            this.labelsal.Name = "labelsal";
+            this.labelsal.Size = new System.Drawing.Size(102, 20);
+            this.labelsal.TabIndex = 2;
+            this.labelsal.Text = "EmployeeSal";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(220, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(220, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // btnwrite
+            // 
+            this.btnwrite.Location = new System.Drawing.Point(63, 275);
+            this.btnwrite.Name = "btnwrite";
+            this.btnwrite.Size = new System.Drawing.Size(90, 47);
+            this.btnwrite.TabIndex = 6;
+            this.btnwrite.Text = "Write";
+            this.btnwrite.UseVisualStyleBackColor = true;
+            this.btnwrite.Click += new System.EventHandler(this.btnwrite_Click);
+            // 
+            // btnread
+            // 
+            this.btnread.Location = new System.Drawing.Point(220, 275);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(100, 47);
+            this.btnread.TabIndex = 7;
+            this.btnread.Text = "Read";
+            this.btnread.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnread);
+            this.Controls.Add(this.btnwrite);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelsal);
+            this.Controls.Add(this.labelname);
+            this.Controls.Add(this.labelid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelid;
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.Label labelsal;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnwrite;
+        private System.Windows.Forms.Button btnread;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Employeeid.Text
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnwrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"A\DotNet20DecBatch\dept.dat", FileMode.Create, FileAccess.Write);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(Convert.ToInt32(txtEmployeeid.Text));
+                bw.Write(txtEmployeename.Text);
+                bw.Write(txtEmployeesal.Text);
+                bw.Close();
+                fs.Close();
+                MessageBox.Show("Data added to file");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
+}
